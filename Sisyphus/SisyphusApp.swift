7a63607962dfc40ee867ab578e61f0586725ac85
Bruno Lemus <brunolemus553@gmail>
2024-09-28 15:49:30 -0300

    Initial Commit

---
+++ Sisyphus/SisyphusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SisyphusApp.swift
+//  Sisyphus
+//
+//  Created by Bruno Lemus on 28/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SisyphusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
